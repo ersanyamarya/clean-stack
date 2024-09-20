3120abfe1b01fd304911bd0e747089e71f959435
Sanyam Arya <er.sanyam.arya@gmail.com>
2024-09-20 17:45:20 +0200

    Update cSpell words and refactor telemetry initialization and logging setup

--- infra/backend-telemetry/src/lib/backend-telemetry.ts
+++ infra/backend-telemetry/src/lib/backend-telemetry.ts
@@ -14,6 +14,23 @@ export type TelemetryConfig = {
   collectorUrl: string;
 };
 
+/**
+ * Initializes the telemetry SDK and main logger for the backend service.
+ *
+ * @param {TelemetryConfig} config - The configuration object for telemetry setup.
+ * @returns {{ telemetrySdk: NodeSDK, mainLogger: pino.Logger }} An object containing the initialized telemetry SDK and main logger.
+ *
+ * @remarks
+ * This function sets up the telemetry SDK using the provided configuration, including
+ * resource attributes, trace and metric exporters, and auto-instrumentations. It also
+ * initializes the main logger using the 'pino' library with a 'pino-pretty' transport,
+ * configured to display debug level messages with colorized output.
+ *
+ * The logger is instantiated only after the instrumentation has started to ensure
+ * that all necessary configurations and dependencies are properly set up. If the
+ * logger is instantiated before the instrumentation, it may not function correctly
+ * due to missing or incomplete setup.
+ */
 export function initTelemetry(config: TelemetryConfig) {
   const telemetrySdk = new NodeSDK({
     resource: new Resource({
@@ -37,16 +54,6 @@ export function initTelemetry(config: TelemetryConfig) {
 
   telemetrySdk.start();
 
-  /**
-   * Initializes the main logger using the 'pino' library with a 'pino-pretty' transport.
-   * The logger is configured to display debug level messages with colorized output.
-   *
-   * @remarks
-   * The logger is instantiated only after the instrumentation has started to ensure
-   * that all necessary configurations and dependencies are properly set up. If the
-   * logger is instantiated before the instrumentation, it may not function correctly
-   * due to missing or incomplete setup.
-   */
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const mainLogger = require('pino')({
     level: 'debug',
