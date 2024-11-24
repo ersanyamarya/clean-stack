061a88105f0f534bf5c14b56574e5ce92d0ffc92
Sanyam Arya <er.sanyam.arya@gmail.com>
2024-11-24 19:50:16 +0100

    feat(frontend-libs): Add new components for UI
    - Added new components for Alert, Button, Card, Chart, HoverCard
    - Implemented styling and functionality for each component
    - Updated TypeScript definitions for components
    
    Closes #456

--- frontend-libs/utils/src/lib/utils.ts
+++ frontend-libs/utils/src/lib/utils.ts
@@ -1,6 +1,15 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+/**
+ * The function `cn` in TypeScript merges multiple class values using `clsx` and `twMerge`.
+ * @param {ClassValue[]} inputs - The `inputs` parameter in the `cn` function is an array of
+ * `ClassValue` types. These can be strings, objects, arrays, or falsy values that represent CSS
+ * classes to be conditionally applied to an element.
+ * @returns The `cn` function is returning the result of merging the class names passed as arguments
+ * using the `clsx` function and then applying Tailwind CSS utility classes using the `twMerge`
+ * function.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
