7021c1ad308d3d0dc766ee9fc4bfc2066bedf89e
Sanyam Arya <er.sanyam.arya@gmail.com>
2024-12-08 21:47:47 +0100

    refactor(ui): standardize import statements and update component structure

--- apps/web-ui/src/@types/resources.d.ts
+++ apps/web-ui/src/@types/resources.d.ts
@@ -1,12 +1,12 @@
 interface Resources {
-  "common": {
-    "locale": "eng",
-    "title": "Hello, World!",
-    "HomePage": {
-      "title": "Home Page",
-      "description": "This is the home page."
-    }
-  }
+  common: {
+    locale: 'eng';
+    title: 'Hello, World!';
+    HomePage: {
+      title: 'Home Page';
+      description: 'This is the home page.';
+    };
+  };
 }
 
 export default Resources;
