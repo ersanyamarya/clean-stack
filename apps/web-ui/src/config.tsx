6334af0b69adf898f2533e5372f876860e4eafaf
Sanyam Arya <er.sanyam.arya@gmail.com>
2024-12-08 21:46:42 +0100

    feat(web-ui): add AppWrite configuration and update dependencies

--- apps/web-ui/src/config.tsx
+++ apps/web-ui/src/config.tsx
@@ -16,3 +16,13 @@ export const app: AppConfig = {
   appName: import.meta.env.VITE_APP_NAME,
   appVersion: import.meta.env.VITE_APP_VERSION,
 };
+
+type AppWriteConfig = {
+  endpoint: string;
+  projectId: string;
+};
+
+export const appWriteConfig: AppWriteConfig = {
+  endpoint: import.meta.env.VITE_APPWRITE_ENDPOINT,
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+};
--- apps/web-ui/vite.config.ts
+++ apps/web-ui/vite.config.ts
@@ -1,9 +1,11 @@
-/// <reference types='vitest' />
+/// <reference types="vitest" />
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 // import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+// import { join } from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/web-ui',
