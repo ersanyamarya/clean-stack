4ab1a2432779e41af1f03f8e20daf3b908213d18
Sanyam Arya <er.sanyam.arya@gmail.com>
2024-12-08 15:56:16 +0100

    feat(telemetry): integrate web vitals metrics and enhance OTLP exporter setup

--- frontend-libs/frontend-telemetry/src/lib/frontend-telemetry.ts
+++ frontend-libs/frontend-telemetry/src/lib/frontend-telemetry.ts
@@ -1,21 +1,42 @@
-import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+import { diag, DiagConsoleLogger, DiagLogLevel, metrics } from '@opentelemetry/api';
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ERROR);
+
+import { onCLS, onINP, onLCP, onTTFB } from 'web-vitals';
 
 import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
 import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { B3Propagator } from '@opentelemetry/propagator-b3';
 import { Resource } from '@opentelemetry/resources';
+import { MeterProvider, PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
 import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
-
 export type FETelemetryConfig = {
   appName: string;
   appVersion: string;
   collectorUrl: string;
   initiateTelemetry?: boolean;
+};
+
+const createExporters = (collectorUrl: string): { otlpExporter: OTLPTraceExporter; otlpMetricExporter: OTLPMetricExporter } => {
+  const otlpExporter = new OTLPTraceExporter({
+    url: `${collectorUrl}/v1/traces`,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const otlpMetricExporter = new OTLPMetricExporter({
+    url: `${collectorUrl}/v1/metrics`,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return { otlpExporter, otlpMetricExporter };
 };
 
 export const initFETelemetry = ({ appName, appVersion, collectorUrl, initiateTelemetry = true }: FETelemetryConfig): void => {
@@ -29,27 +50,31 @@ export const initFETelemetry = ({ appName, appVersion, collectorUrl, initiateTel
     [ATTR_SERVICE_VERSION]: appVersion,
   });
 
-  // Initialize the OTLP exporter
-  const otlpExporter = new OTLPTraceExporter({
-    url: `${collectorUrl}/v1/traces`,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  const { otlpExporter, otlpMetricExporter } = createExporters(collectorUrl);
 
-  const provider = new WebTracerProvider({
+  const tracingProvider = new WebTracerProvider({
     resource: resource,
     spanProcessors: [new BatchSpanProcessor(otlpExporter)],
   });
-
-  const contextManager = new ZoneContextManager();
-  const propagator = new B3Propagator();
-
-  // Register the tracer provider with propagator
-  provider.register({
-    contextManager,
-    propagator,
+  tracingProvider.register({
+    contextManager: new ZoneContextManager(),
+    propagator: new B3Propagator(),
   });
+
+  const meterProvider = new MeterProvider({
+    resource: resource,
+    readers: [
+      new PeriodicExportingMetricReader({
+        exporter: otlpMetricExporter,
+        exportIntervalMillis: 30 * 1000, // 30 seconds
+      }),
+    ],
+    mergeResourceWithDefaults: true,
+  });
+
+  metrics.setGlobalMeterProvider(meterProvider);
+
+  initWebVitals(meterProvider);
 
   registerInstrumentations({
     instrumentations: [
@@ -58,13 +83,11 @@ export const initFETelemetry = ({ appName, appVersion, collectorUrl, initiateTel
           enabled: true,
           clearTimingResources: true,
           propagateTraceHeaderCorsUrls: [/.*/], // Match all URLs
-          // applyCustomAttributesOnSpan: span => {
-          //   span.setAttribute('client.name', appName);
-          //   // Debug log the trace context
-          //   const currentContext = context.active();
-          //   const spanContext = trace.getSpan(currentContext)?.spanContext();
-          //   console.debug('Current trace context:', spanContext);
-          // },
+          applyCustomAttributesOnSpan: span => {
+            const spanData = Object.assign({}, span) as any;
+            const url = spanData.attributes?.['http.url'];
+            span.updateName(createSpanName(url, spanData.name));
+          },
         },
         '@opentelemetry/instrumentation-xml-http-request': {
           enabled: true,
@@ -75,9 +98,38 @@ export const initFETelemetry = ({ appName, appVersion, collectorUrl, initiateTel
         '@opentelemetry/instrumentation-user-interaction': { enabled: true },
       }),
     ],
-    tracerProvider: provider,
+    tracerProvider: tracingProvider,
+    meterProvider: meterProvider,
   });
 
   // Debug log to confirm initialization
   console.debug('OpenTelemetry initialization completed');
 };
+
+function initWebVitals(meterProvider: MeterProvider) {
+  function sendMetric(metric: any) {
+    const { name, value, rating } = metric;
+    console.log(`Web Vitals - ${name}: ${value} (${rating})`);
+
+    const meter = meterProvider.getMeter('web-vitals');
+
+    const vitalMetric = meter.createHistogram(`web_vital_${name.toLowerCase()}`, {
+      description: `Web Vital - ${name}`,
+    });
+
+    vitalMetric.record(value, {
+      rating: rating,
+      metric_name: name,
+    });
+  }
+
+  onCLS(sendMetric);
+  onLCP(sendMetric);
+  onTTFB(sendMetric);
+  onINP(sendMetric);
+}
+
+function createSpanName(url: string, name: string) {
+  const { host, pathname } = new URL(url);
+  return `${name} ${host} ${pathname}`;
+}
