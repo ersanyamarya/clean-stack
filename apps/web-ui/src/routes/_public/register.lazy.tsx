6de01eee0b8dbd92280df36518938b1a0e899fa5
Sanyam Arya <er.sanyam.arya@gmail.com>
2024-12-27 14:49:36 +0530

    fix(routes): Correct syntax in register.lazy.tsx for consistency

--- apps/web-ui/src/routes/_public/register.lazy.tsx
+++ apps/web-ui/src/routes/_public/register.lazy.tsx
@@ -1,9 +1,9 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { createLazyFileRoute } from '@tanstack/react-router';
 
 export const Route = createLazyFileRoute('/_public/register')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/_public/register"!</div>
+  return <div>Hello "/_public/register"!</div>;
 }
